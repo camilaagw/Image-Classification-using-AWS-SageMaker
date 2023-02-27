ea6d8fd93cef7f6b42515c01244b8ad37549ecc5
Camila Gonzalez Williamson <camilaagw@gmail.com>
2023-02-27 21:20:58 +0000

    Update code

--- Tips.md
+++ Tips.md
@@ -1,5 +1,8 @@
 # Tips to save your credit and time when doing projects 3 and 4
- If you restart your development notebook kernel, instead of re-running the costly training jobs or hyperparameter tuning jobs from scratch you can load (attach) completed jobs to a variable.
+
+Credit: Fady SL.
+
+### If you restart your development notebook kernel, instead of re-running the costly training jobs or hyperparameter tuning jobs from scratch you can load (attach) completed jobs to a variable.
 For training jobs, use this code to attach a complted training job:
 `estimator = PyTorch.attach(<training_job_name>)`
 
@@ -9,6 +12,7 @@ For hyperparameter tuning jobs:
 If you have an already deployed endpoint and you want to use it, you can attach it using the following code:
 `predictor = sagemaker.pytorch.model.PyTorchPredictor(endpoint_name)`
 
+### Test your code in jupyter notebooks before sumitting complete jobs
 Instead of launching a training job with a training script, you can develop your train_model.py script locally inside a jupyter notebook. You can simulate the training instance environment variables and the command line arguments using the following code:
 ```python
 import sys
@@ -30,11 +34,12 @@ os.environ['SM_CHANNEL_TEST'] = "./dogImages/test"
 os.environ['SM_MODEL_DIR'] = "/opt/ml/model"
 
 
-# Crate a model directory and take ownership 
+# Create a model directory and take ownership 
 !sudo mkdir /opt/ml/model
 !sudo chown -R 1000:1000 /opt/ml/model
 ```
 
+### Subset the data
 For the starting phase of the creating your project, you just want to focus on the correctness of your code. Use a subset of the dataset to speed your training and testing. You can use a break statement inside the training or testing loop. For example:
 ```
 for step, (inputs, labels) in enumerate(data_loader):
@@ -45,3 +50,12 @@ for step, (inputs, labels) in enumerate(data_loader):
     if running_samples>(proportion*dataset_length):
         break
 ```
+
+### Use debugger and profiling without the estimator
+
+You can use these two configuration files in your local Jupyter notebook instance to run debugger and profiler and simulate passing configurations to the training machine.
+ * debughookconfig.json : extracted debugging configuration file passed to the training job container, extracted from /opt/ml/input/config
+ * profilerconfig.json : extracted profiling configuration file passed to the training job container, extracted from /opt/ml/input/config
+To get these parameters, the following line was inserted at the end of the model training script `pytorch_cifar_profiling.py` (see lesson examples):
+`os.system("tar cvzf /opt/all-data.tar.gz /opt/ml/;cp /opt/all-data.tar.gz /opt/ml/output/data")`
+It compress all the data inside `/opt/ml` and sends the compressed archive to the instance output directory, then the output is copied to default S3 bucket output location.
--- hpo.py
+++ hpo.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 import torch
 import torch.nn as nn
@@ -8,43 +9,133 @@ import torchvision.transforms as transforms
 
 import argparse
 
-def validate(model, validation_loader):
-    '''
-    TODO: Complete this function that can take a model and a 
-          testing data loader and will get the test accuray/loss of the model
-          Remember to include any debugging/profiling hooks that you might need
-    '''
-    pass
+TRAIN ='train'
+VALIDATION = 'val'
 
-def train(model, train_loader, criterion, optimizer, epoch):
-    
+MAX_SAMPLES_PROPORTION = 0.2
+THESHOLD_LOGGING_SAMPLES = 2000
 
+def log_metrics(loss, running_corrects, running_samples, total_samples):
+    accuracy = running_corrects / running_samples
+    print("Images [{}/{} ({:.0f}%)] Loss: {:.3f} Accuracy: {}/{} ({:.3f}%)".format(
+        running_samples,
+        total_samples,
+        100.0 * (running_samples / total_samples),
+        loss.item(),
+        running_corrects,
+        running_samples,
+        100.0 * accuracy,
+        )
+    )
+
+
+def validate(model, validation_loader, criterion):
+    model.eval()
+    running_loss = 0
+    running_corrects = 0
+    running_samples = 0
+    total_samples = len(validation_loader.dataset)
+
+    for inputs, labels in validation_loader:
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+
+        _, preds = torch.max(outputs, 1)
+        running_loss += loss.item() * inputs.size(0)
+        running_corrects += torch.sum(preds == labels.data).item()
+        running_samples += len(inputs)
+        if running_samples % THESHOLD_LOGGING_SAMPLES == 0:
+            log_metrics(loss, running_corrects, running_samples, total_samples)
+            
+        #NOTE: Comment lines below to train and test on whole dataset
+        if running_samples > (MAX_SAMPLES_PROPORTION * total_samples):
+            break
+
+    epoch_loss = running_loss / running_samples
+    epoch_acc = running_corrects / running_samples
+    print(f"Phase validation, Epoc loss {epoch_loss:.3f}, Epoc accuracy {100*epoch_acc:.3f}%")
+    return epoch_loss
+
+
+def train(model, train_loader, criterion, optimizer):
+    model.train()
+
+    running_loss = 0.0
+    running_corrects = 0
+    running_samples = 0
+    total_samples = len(train_loader.dataset)
+
+    for inputs, labels in train_loader:
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        _, preds = torch.max(outputs, 1)
+        running_loss += loss.item() * inputs.size(0)
+        running_corrects += torch.sum(preds == labels.data).item()
+        running_samples += len(inputs)
+        if running_samples % THESHOLD_LOGGING_SAMPLES == 0:
+            log_metrics(loss, running_corrects, running_samples, total_samples)
+
+        #NOTE: Comment lines below to train and test on whole dataset
+        if running_samples > (MAX_SAMPLES_PROPORTION * total_samples ):
+            break
+
+    epoch_loss = running_loss / running_samples
+    epoch_acc = running_corrects / running_samples
+    print(f"Phase training, Epoc loss {epoch_loss:.3f}, Epoc accuracy {100*epoch_acc:.3f}%")
+    return epoch_loss
+
+def train_with_early_stopping(model, datasets_loader, epochs, loss_criterion, optimizer):
+    print(f"Training Model on {MAX_SAMPLES_PROPORTION*100}% of the Dataset")
+    best_loss = 1e6
     
+    for epoch in range(1, epochs + 1):
+        print(f"Epoch {epoch} ...")
+        _ = train(model, datasets_loader[TRAIN], loss_criterion, optimizer)
+        validate_epoch_loss = validate(model, datasets_loader[VALIDATION], loss_criterion)
+        print(validate_epoch_loss, best_loss)
+        if validate_epoch_loss < best_loss:
+            best_loss = validate_epoch_loss
+        else:
+            print('Loss of validation model started to increase')
+            break
+            
 def net(num_classes: int):
     '''Initializes a pretrained model'''
     model = models.resnet50(pretrained=True)
 
-    # Freeze training of the upstream layers
+    # Freeze training of the convolutional layers
     for param in model.parameters():
         param.requires_grad = False   
 
     # Override the last layer to adjust it to our problem
     num_features=model.fc.in_features
-    model.fc = nn.Sequential(nn.Linear(num_features, num_classes))
+    model.fc = nn.Sequential(
+        nn.Linear(num_features, 1024),
+        nn.ReLU(inplace=True),
+        nn.Linear(1024, num_classes)
+    )
     
     return model
 
-def create_data_loaders(data_dir: str, batch_size: int, train_suffix: str ='train', val_suffix : str = 'val'):
+def create_data_loaders(train_data_dir: str, valid_data_dir: str, batch_size: int):
     '''Create pytorch data loaders'''
+     
+    data_dir = {TRAIN: train_data_dir, VALIDATION: valid_data_dir}
     
     data_transforms = {
-        train_suffix: transforms.Compose([
-            transforms.RandomResizedCrop(224),
+        TRAIN: transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
             transforms.RandomHorizontalFlip(),
             transforms.ToTensor(),
             transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
         ]),
-        val_suffix: transforms.Compose([
+        VALIDATION: transforms.Compose([
             transforms.Resize(256),
             transforms.CenterCrop(224),
             transforms.ToTensor(),
@@ -54,48 +145,35 @@ def create_data_loaders(data_dir: str, batch_size: int, train_suffix: str ='trai
    
     image_datasets = {}
     dataloaders = {}
-    dataset_sizes = {}
-    for x in [train_suffix, val_suffix]:
+    for x in [TRAIN, VALIDATION]:
         
-        image_datasets[x] = datasets.ImageFolder(os.path.join(data_dir, x), data_transforms[x])
-        dataset_sizes[x] = len(image_datasets[x])
+        image_datasets[x] = torchvision.datasets.ImageFolder(data_dir[x], data_transforms[x])
         dataloaders[x] = torch.utils.data.DataLoader(
-            image_datasets[x], batch_size=batch_size, shuffle=True, num_workers=4
-        )
+            image_datasets[x], batch_size=batch_size, shuffle=True, num_workers=2)
         
-    class_names = image_datasets['train'].classes
-    
-    return dataloaders[train_suffix], dataloaders[val_suffix], len(class_names)
+    return dataloaders
 
+def get_num_classes(dataloader) -> int:
+        return len(dataloader[TRAIN].dataset.classes)
+    
+def save_model(model, model_dir):
+    path = os.path.join(model_dir, "model.pth")
+    print(f"Saving the model to path {path}")
+    torch.save(model.state_dict(), path)
+    
 def main(args):
-
-    train_loader, valid_loader, num_classes = create_data_loaders(ags.data_dir, ags.batch_size, 'train', 'valid')
     
+    dataset_loaders = create_data_loaders(args.data_dir_train, args.data_dir_validation, args.batch_size)
+    
+    num_classes = get_num_classes(dataset_loaders)
     model=net(num_classes)
     loss_criterion = nn.CrossEntropyLoss()
     optimizer =  optim.Adam(model.fc.parameters(), lr=args.lr)
-
-    for epoch in range(1, args.epochs + 1):
-        train(model, train_loader, loss_criterion, optimizer, epoch)
-        validate(model, test_loader)
-
     
-    torch.save(model, PATH) #TODO  torch.save(model.state_dict(), "mnist_cnn.pt")
+    train_with_early_stopping(model, dataset_loaders, args.epochs, loss_criterion, optimizer)
+    save_model(model, args.model_dir)
     
-# def model_fn(model_dir):
-#     model = Net()
-#     with open(os.path.join(model_dir, "model.pth"), "rb") as f:
-#         model.load_state_dict(torch.load(f))
-#     return model
 
-
-# def save_model(model, model_dir):
-#     logger.info("Saving the model.")
-#     path = os.path.join(model_dir, "model.pth")
-#     torch.save(model.cpu().state_dict(), path)
-
-
-    
 if __name__=='__main__':
     parser=argparse.ArgumentParser(description="Training Job for Hyperparameter tuning")
     
@@ -106,34 +184,23 @@ if __name__=='__main__':
         metavar="N",
         help="input batch size for training (default: 64)",
     )
-    parser.add_argument(
-        "--test-batch-size",
-        type=int,
-        default=1000,
-        metavar="N",
-        help="input batch size for testing (default: 1000)",
-    )
+
     parser.add_argument(
         "--epochs",
         type=int,
-        default=2,
+        default=14,
         metavar="N",
         help="number of epochs to train (default: 14)",
     )
     parser.add_argument(
         "--lr", type=float, default=1.0, metavar="LR", help="learning rate (default: 1.0)"
     )
-    parser.add_argument(
-        "--momentum", type=float, default=0.5, metavar="M", help="SGD momentum (default: 0.5)"
-    )
-    
+
     # Container environment
-#     parser.add_argument("--hosts", type=list, default=json.loads(os.environ["SM_HOSTS"]))
-#     parser.add_argument("--current-host", type=str, default=os.environ["SM_CURRENT_HOST"])
-#     parser.add_argument("--model-dir", type=str, default=os.environ["SM_MODEL_DIR"])
-    parser.add_argument("--data-dir", type=str, default=os.environ["SM_CHANNEL_TRAINING"])
-#     parser.add_argument("--num-gpus", type=int, default=os.environ["SM_NUM_GPUS"])
-    
+    parser.add_argument("--model-dir", type=str, default=os.environ["SM_MODEL_DIR"])
+    parser.add_argument("--data-dir-train", type=str, default=os.environ["SM_CHANNEL_TRAINING"])
+    parser.add_argument("--data-dir-validation", type=str, default=os.environ["SM_CHANNEL_VALIDATION"])
+
     args = parser.parse_args()
     
     main(args)
